adb6c79d2cd5f2352a58e8b908af2a28f67fc1fc
Chris Daley <chebizarro@gmail.com>
2020-03-28 08:33:14 -0700

    quasar version
    Signed-off-by: Chris Daley <chebizarro@gmail.com>

---
+++ redshed/src/App.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import Vue from 'vue';
+  import { Component } from 'vue-property-decorator';
+
+  @Component
+  export default class App extends Vue {
+
+    async created(): Promise<void> {
+      /**
+       * Order of startup is important here.
+       * Startup functions may register eventbus listeners.
+       * If they do so after the eventbus started,
+       * they will miss the first (immediate) data push.
+       */
+      await Vue.$startup.start();
+      await Vue.$eventbus.start();
+    }
+  }
+</script>
+
+<template>
+  <div id="q-app">
+    <router-view />
+    <Watchers />
+  </div>
+</template>
---
+++ redshed/src/router.ts
@@ -0,0 +1,24 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes: [
+    {
+      path: '/',
+      component: () => import('layouts/MainLayout.vue'),
+      children: [
+        { path: '', component: () => import('pages/Index.vue') }
+      ]
+    },
+    { // Always leave this as last one
+      path: '*',
+      component: () => import('pages/Error404.vue')
+    },
+  ],
+  mode: 'history',
+  base: '/ui/',
+});
+
+export default router;
