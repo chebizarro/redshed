e70df2226d84d2550fc50e1b8b0bc5c2f6d0f6cb
Chris Daley <chebizarro@gmail.com>
2020-04-05 20:56:51 -0700

    Server mods
    Signed-off-by: Chris Daley <chebizarro@gmail.com>

--- pkg/server/routes/misc.go
+++ pkg/server/routes/misc.go
@@ -1,8 +1,6 @@
 package routes
 
 import (
-	"github.com/chebizarro/redshed/internal/handlers"
-	"github.com/chebizarro/redshed/internal/handlers/auth/middleware"
 	"github.com/chebizarro/redshed/internal/orm"
 	"github.com/chebizarro/redshed/pkg/utils"
 	"github.com/gin-gonic/gin"
@@ -11,8 +9,10 @@ import (
 // Misc routes
 func Misc(cfg *utils.ServerConfig, r *gin.Engine, orm *orm.ORM) error {
 	// Simple keep-alive/ping handler
-	r.GET(cfg.VersionedEndpoint("/ping"), handlers.Ping())
-	r.GET(cfg.VersionedEndpoint("/secure-ping"),
-		middleware.Middleware(cfg.VersionedEndpoint("/secure-ping"), cfg, orm), handlers.Ping())
+	//r.GET(cfg.VersionedEndpoint("/ping"), handlers.Ping())
+	/*
+		r.GET(cfg.VersionedEndpoint("/secure-ping"),
+			middleware.Middleware(cfg.VersionedEndpoint("/secure-ping"), cfg, orm), handlers.Ping())
+	*/
 	return nil
 }
